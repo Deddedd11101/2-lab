6f15045b0c8213779c121d8f00be634fd801f565
Востриков Антон <93368273+Deddedd11101@users.noreply.github.com>
2021-11-26 01:35:57 +0300

    Add files via upload

---
+++ 2num.cpp
@@ -0,0 +1,22 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	if (e1) if (e2) S1; else S2;
+
+	if (e1) { if (e2) S1; else S2; }
+
+	if (e1) { if (e2) S1; }
+	else S2;
+
+	if (e1) if (e2) S1; else; else S2;
+
+	if (e1) if (e2) S1; else S2; else; 
+	// эквивалентны 1 2 и 3 
+	// эквивалентны 4 и 5 (при записи допущены ошибки)
+}
+
+
